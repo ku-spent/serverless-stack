949c849df5156d4bacbe3093cb27832563a20c66
thtiiz <thitiwat.tha@ku.th>
2021-03-15 13:09:46 +0700

    add filter news feed

--- services/feed-service/src/handlers/news.ts
+++ services/feed-service/src/handlers/news.ts
@@ -1,14 +1,45 @@
 import { Handler } from 'aws-lambda'
-import esb, { boolQuery, matchAllQuery, matchQuery, termQuery } from 'elastic-builder'
+import esb, { boolQuery, matchAllQuery, matchQuery, termQuery, termsQuery } from 'elastic-builder'
 import commonMiddleware from '../libs/commonMiddleware'
 import { esSearch } from '../libs/elasticsearch'
 
 const news: Handler = async (event, context) => {
-  const { from = 0, size = 5, query, queryField = '_' } = event?.queryStringParameters
+  const {
+    from = 0,
+    size = 5,
+    query,
+    queryField = '_',
+    filterCategories,
+    filterSources,
+    filterTags,
+  } = event?.queryStringParameters
+
+  // const filterTags: string[] = []
+  // const filterSources: string[] = ['มติชน']
+  // const filterCategories: string[] = ['การเมือง']
+
+  let filterQuery = boolQuery().must(query ? matchQuery(queryField, query) : matchAllQuery())
+  const filterObj = {
+    category: filterCategories,
+    source: filterSources,
+    tags: filterTags,
+  }
+
+  try {
+    Object.keys(filterObj).map((field) => {
+      if (filterObj[field]) {
+        const parsedValues: string[] = JSON.parse(filterObj[field])
+        if (!(parsedValues instanceof Array)) return { statusCode: 400 }
+        filterQuery = filterQuery.mustNot(termsQuery().field(field).values(parsedValues))
+      }
+    })
+  } catch {
+    return { statusCode: 400 }
+  }
 
   const body = esb
     .requestBodySearch()
-    .query(query ? matchQuery(queryField, query) : matchAllQuery())
+    .query(filterQuery)
     .sort(esb.sort('pubDate', 'desc'))
     .from(from)
     .size(size)
